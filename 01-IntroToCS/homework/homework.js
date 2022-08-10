d535c9efd93b254c94aad8dfd07d1c424dbfbcd2
nakiche <tommynaquiche@hotmail.com>
2022-08-10 11:54:03 -0500

    Subiendo la tarea uno con el código depurado

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,48 +2,31 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-var separarCadena = num.split("").reverse().join("");
-//var invertirArreglo = separarCadena.reverse(); 
-//var unirArreglo = invertirArreglo.join("");
-
-var valor = 0;
+var array = num.split("").reverse().join("");
 var sumador = 0;
-for (let i=0 ; i < separarCadena.length; i++)
+
+for (let i=0 ; i < array.length; i++)
   {
-    sumador += (separarCadena[i] * (Math.pow(2,i)));;
+    sumador += (array[i] * (Math.pow(2,i)));;
   }
 return sumador;
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca  
-  var resto = 0;
-  var resultado = num;
-  var c = 0;
   var arreglo = [];
   
-  while (resultado > 0) 
+  while (num > 0) 
   {
-     if (c > 0) {resto=resultado % 2;}
-     
-     resultado= Math.floor(resultado/2);
- 
-     if (c < 1)
-     {
-       resto = num % 2;
-       c++;
-     }         
-
-     arreglo.push(resto);
+     arreglo.push(num % 2);     
+     num = Math.floor(num / 2);
   }
-
+  
   return arreglo.reverse().join("");
-  //var unirArreglo = invertirArreglo.join("");
-  //return invertirArreglo;
+  
 }
-
 
 module.exports = {
   BinarioADecimal,
   DecimalABinario,
 }
+
