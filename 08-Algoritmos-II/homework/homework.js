e89b8856310ce9e07d3e9c0455fb7d2eb9941ca9
nakiche <tommynaquiche@hotmail.com>
2022-08-30 23:04:10 -0500

    08-Algoritmos-II

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -7,7 +7,7 @@ function quickSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
-  if (array.length<=1){
+  if (array.length <= 1){
   return array;
   }
   else
@@ -36,7 +36,40 @@ function mergeSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+if (array.length <= 1){
+  return array;
 }
+else
+{ 
+  var mitad = Math.floor(array.length/2);
+  var izq = array.slice(0,mitad);
+  var der = array.slice(mitad,array.length) //tamben puede ser solo slice(mitad)
+}
+
+return mover(mergeSort(izq),mergeSort(der));
+
+}
+
+function mover(izq,der){
+  var indiceIzq = 0;
+  var indiceDer = 0;
+  var nuevoArray = [];
+
+while(indiceIzq < izq.length && indiceDer < der.length){
+  if(izq[indiceIzq]<der[indiceDer]){
+    nuevoArray.push(izq[indiceIzq]);
+    indiceIzq++;
+  }
+  else
+  {
+    nuevoArray.push(der[indiceDer]);
+    indiceDer++
+  } 
+}
+
+  return nuevoArray.concat(izq.slice(indiceIzq)).concat(der.slice(indiceDer))
+}
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
